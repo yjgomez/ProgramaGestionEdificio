ea5e76c68f7dca7ff1f02c7b6e6f7afd2bbf10bb
yjgomez <116039364+yjgomez@users.noreply.github.com>
2024-09-15 18:08:04 -0300

    first commit

---
+++ src/main/java/com/grupo11/ProgramaGestionEdificio/ApiGestionEdificioApplication.java
@@ -0,0 +1,15 @@
+package com.grupo11.ProgramaGestionEdificio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestionEdificioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGestionEdificioApplication.class, args);
+		
+		System.out.println(" hello world");
+	}
+
+}
---
+++ src/test/java/com/grupo11/ProgramaGestionEdificio/ApiGestionEdificioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo11.ProgramaGestionEdificio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestionEdificioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
